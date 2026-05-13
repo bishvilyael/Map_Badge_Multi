926ad9b93141f8762af13e2fbb92478bbf81eed1
MaayanChr <maayan.chr@gmail.com>
2026-05-13 13:05:09 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
+async function testLoadBadgesIndex() {
+  const response = await fetch("json/badges.json", { cache: "no-store" });
+
+  if (!response.ok) {
+    throw new Error("badges.json לא נטען");
+  }
+
+  const badges = await response.json();
+
+  if (!Array.isArray(badges)) {
+    throw new Error("badges.json לא תקין");
+  }
+
+  console.log("badges.json נטען בהצלחה:", badges);
+}
+
 (async function () {
   try {
+    await testLoadBadgesIndex();
+
     await loadBadgeTitle();
     await loadBadgeGeoJson();
   } catch (err) {
