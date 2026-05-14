6397e23bfa83b5927b3cce475c27d74b5e9a7451
MaayanChr <maayan.chr@gmail.com>
2026-05-14 15:44:55 +0300

    Update helpers.js

--- js/helpers.js
+++ js/helpers.js
@@ -55,25 +55,3 @@ function createMarkerIcon(labelText, isMain) {
     popupAnchor: [0, -11]
   });
 }
-function extractPointDetailsFromDescriptionY(html) {
-  const temp = document.createElement("div");
-  temp.innerHTML = html || "";
-
-  function getValue(label) {
-    const text = temp.innerHTML;
-    const re = new RegExp(`<b>\\s*${label}\\s*:\\s*<\\/b>\\s*([^<]*)`, "i");
-    const m = text.match(re);
-    return m ? m[1].trim() : "";
-  }
-
-  const fbLink = Array.from(temp.querySelectorAll("a"))
-    .map(a => a.getAttribute("href") || "")
-    .find(href => /facebook\.com/i.test(href)) || "";
-
-  return {
-    name: getValue("שם"),
-    date: getValue("תאריך"),
-    place: getValue("מקום"),
-    fbUrl: fbLink
-  };
-}
